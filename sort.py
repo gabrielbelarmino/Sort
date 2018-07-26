e264501294e1b0c06e5d6a0d6ee254f3e81b62f9
Gabriel <gabrielbelarmino1995@gmail.com>
2018-07-26 09:37:53 -0300

    Comentando Código

--- sort.py
+++ sort.py
@@ -1,23 +1,32 @@
 import sys
 
 def selection(List):
+#   varre vetor a partir do item inicial    
     for indice in range(0,len(List)):
-        indic_min = indice
+#       associa indice do item menor        
+        indic_men = indice
+#       laco verificador do menor item        
         for j in range(indice+1,len(List)):
-            if List[j] < List[indic_min]:
-                indic_min = j
-        if List[indice] != List[indic_min]:
-            temp = List[indice]
-            List[indice] = List[indic_min]
-            List[indic_min] = temp
+            if List[j] < List[indic_men]:
+                indic_men = j
+#       troca caso lista esteja desordenada                
+        if List[indice] != List[indic_men]:
+            auxiliar = List[indice]
+            List[indice] = List[indic_men]
+            List[indic_men] = auxiliar
 
 def insertion(List):
+#   inicia pelo segundo (pivo) elemento comparando aos antecessores   
     for indice in range(1,len(List)):
+#       atualiza o pivo        
         pivo = List[indice]
         j = indice - 1
+#       laco compara com todos os antecessores        
         while j >= 0 and List[j] > pivo:
+#           Se o antecessor for maior realiza a troca            
             List[j+1] = List[j]
             j = j-1
+#       encontra o local do pivo apos o laco    
         List[j+1] = pivo
 
 def main():
@@ -30,7 +39,7 @@ def main():
         selection(List)
         print(List)
     else:
-        print("Segundo argumento invalido")    
+        print("Selecao de algoritimo invalido!!")    
     
 if __name__ == '__main__':
     main()          
